18f980ba434ba23adeb2f6de533c4efbf426ee48
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-11 18:44:26 -0700

    dispRooms <- sb | format list element | renderFunc

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { getRooms } from './fetch-utils.js';
+import { renderRoom } from './render-utils.js';
 
 /* Get DOM Elements */
+const errorDisplay = document.getElementById('error-display');
+const roomsList = document.getElementById('rooms-list');
 
 /* State */
+let error = null;
+let rooms = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getRooms();
+
+    error = response.error;
+    rooms = response.data;
+
+    if (error) {
+        displayError();
+    } else {
+        displayRooms();
+    }
+});
 
 /* Display Functions */
+function displayError() {
+    if (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
+
+function displayRooms() {
+    roomsList.innerHTML = '';
+
+    for (const room of rooms) {
+        const roomEl = renderRoom(room);
+        roomsList.append(roomEl);
+    }
+}
--- render-utils.js
+++ render-utils.js
@@ -28,20 +28,40 @@ export function renderComment(comment) {
 
 export function renderRoom(room) {
     const li = document.createElement('li');
+    li.classList.add('room-item');
 
     const a = document.createElement('a');
     a.href = `/post/?id=${room.id}`;
 
+    const div = document.createElement('div');
+    div.classList.add('description-image-container');
+
+    const titleCatDiv = document.createElement('div');
+    titleCatDiv.classList.add('title-category-container');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('room-title');
+    h3.textContent = room.title;
+
+    const span = document.createElement('span');
+    span.classList.add('room-category');
+    span.textContent = room.category;
+
     const image = document.createElement('img');
     image.src = room.image;
 
-    const h2 = document.createElement('h2');
-    h2.textContent = room.title;
+    // if (image.src < )
 
     const p = document.createElement('p');
+    p.classList.add('room-description');
     p.textContent = room.body;
 
-    a.append(h2, image, p);
+    titleCatDiv.append(h3, span);
+
+    div.append(titleCatDiv, image);
+
+    a.append(div, p);
+
     li.append(a);
 
     return li;

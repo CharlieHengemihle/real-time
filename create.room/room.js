1041e7ff37695f2a747f92addca5cee4babe1d57
King Kong <king-kong@gmail.com>
2022-10-12 09:36:22 -0700

    image input for posts display

--- create.room/room.js
+++ create.room/room.js
@@ -1,21 +1,45 @@
+import { uploadImage } from '../fetch-utils.js';
 import '/auth/user.js';
 import { createRoom } from '/fetch-utils.js';
 
 /* DOM Elements*/
 const errorDisplay = document.getElementById('error-display');
 const createRoomForm = document.getElementById('create-room-form');
+const imageInput = document.getElementById('image-input');
+const preview = document.getElementById('preview');
+
+preview.classList.add('avatar');
 
 let error = null;
+
+/* event listeners*/
+imageInput.addEventListener('change', () => {
+    const file = imageInput.files[0];
+
+    if (file) {
+        preview.classList.add('avatar');
+        preview.src = URL.createObjectURL(file);
+    } else {
+        preview.src = '../assets/placeholder-image.png';
+    }
+});
 
 createRoomForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(createRoomForm);
+
+    const imageFile = formData.get('image');
+    const imageFolder = Math.floor(Date.now() * Math.random());
+    const imagePath = `rooms/${imageFolder}/${imageFile.title}`;
+    const url = await uploadImage('bucket1', imagePath, imageFile);
+
     const room = {
         title: formData.get('room-name'),
         about: formData.get('room-description'),
         category: formData.get('room-category'),
-        image: formData.get('image'),
+        image: url,
     };
+
     const response = await createRoom(room);
     error = response.error;
     errorDisplay.formData = '';
@@ -29,6 +53,7 @@ createRoomForm.addEventListener('submit', async (e) => {
         location.assign('/');
     }
 });
+
 /* Display Functions */
 function displayError() {
     errorDisplay.textContent = error.message;
